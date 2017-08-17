42eec487da93eebdfe1f02c9f605d864e187ea00
d08ni <d08ni@DESKTOP-LOCQPU1>
2017-08-17 15:03:05 +1000

    Add working for Linkedlist

--- src/LinkedListMultiset.java
+++ src/LinkedListMultiset.java
@@ -1,9 +1,28 @@
 import java.io.PrintStream;
-import java.util.*;
 
 public class LinkedListMultiset<T> extends Multiset<T>
 {
-    //private Integer Head;
+
+    private void delete(Node Temp) {        
+        // When the node to be deleted is the last node
+        if (Temp == Head){
+            // When there is only one node
+            if(Temp.prv == null)
+                Head = null;
+            else{
+                Head = Temp.prv;
+                Temp.prv.nxt = null;            
+            }
+        }
+        //When the node to be deleted is the first node.
+        else if (Temp.prv == null)
+            Temp.nxt.prv =null;
+        else{
+            Temp.prv.nxt = Temp.nxt;
+            Temp.nxt.prv = Temp.prv;
+        }
+    }
+    //The structure of the linkedlist
     private static class Node<T>
     {
         protected T data;
@@ -18,12 +37,12 @@ public class LinkedListMultiset<T> extends Multiset<T>
         }
     }
     Node Head;
+    private int Length;
 	public LinkedListMultiset() {
-            Head = null;            
-		// Implement me!
+            Head = null;
+            Length = 0;
 	} // end of LinkedListMultiset()
-	
-	
+	        
 	public void add(T item) {
             // when there are no elements in Linklist ie when head is null.
             if (Head==null)
@@ -31,36 +50,45 @@ public class LinkedListMultiset<T> extends Multiset<T>
             // If the Linklist already consist of data
             else {
                 Node Temp = new Node(item, null, Head);
+                Head.nxt = Temp;
                 Head = Temp;
             }
-		// Implement me!
 	} // end of add()
-	
-	
+		
 	public int search(T item) {
-		// Implement me!		
-		// default return, please override when you implement this method
-		return 0;
+            Node Temp = Head;
+            int count = 0;
+            for(;Temp != null;Temp = Temp.prv)
+                if (Temp.data.equals(item))
+                    count++;
+		return count;
 	} // end of add()
-	
-	
+		
 	public void removeOne(T item) {
-		// Implement me!
+            Node Temp = Head;
+            for(;Temp != null;Temp = Temp.prv){
+                if (Temp.data.equals(item)){
+                    delete (Temp);
+                    break;  //To delete only once.
+                }                    
+            }
 	} // end of removeOne()
-	
-	
+		
 	public void removeAll(T item) {
-		// Implement me!
-	} // end of removeAll()
-	
+            Node Temp = Head;
+            for(;Temp != null;Temp = Temp.prv)
+                if (Temp.data.equals(item))
+                    delete (Temp);
+	} // end of removeAll()	
 	
 	public void print(PrintStream out) {
             Node Temp = Head;
-            for(;Temp != null;Temp = Temp.prv){
-                out.println(Temp.data+""+printDelim);
-            }
+            //T[] aryTs = new T[10];
             
-		// Implement me!
+            for(;Temp != null;Temp = Temp.prv){
+                //put in array
+                out.println(Temp.data+""+printDelim+""+search((T)Temp.data));
+            }
 	} // end of print()
 	
 } // end of class LinkedListMultiset
